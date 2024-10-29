42949aa566800105af9f658398e63a168dfa7ec7
TheKyleWilliams <101381655+TheKyleWilliams@users.noreply.github.com>
2024-10-29 12:38:19 -0700

    Fix forms.py

--- app/forms.py
+++ app/forms.py
@@ -11,4 +11,23 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
+    # Validation Methods
+    def validate_username(self, username):
+        # checks if username already exists
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError('Username already exists.')
+        
+    def validate_email(self, email):
+        # checks if email already exists
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError('Email already registered')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
